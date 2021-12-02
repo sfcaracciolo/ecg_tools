6bdffee21c175433a76ac14cab3c864d17022a1c
sfcaracciolo <santiago.caracciolo@gmail.com>
2021-12-02 14:49:25 -0300

    return beat_matrix with all beats

--- ecg_tools/utils.py
+++ ecg_tools/utils.py
@@ -3,5 +3,7 @@ import numpy as np
 def sliding_window_from_centers(s: np.ndarray, centers: np.ndarray, size: int) -> tuple:
     onsets = centers - size // 2
     mask = (onsets >= 0) & (onsets <= s.size - size)
-    matrix = np.lib.stride_tricks.sliding_window_view(s, size)[onsets[mask], :]
-    return matrix, onsets, np.logical_not(mask)
+    np.logical_not(mask, out=mask)
+    matrix = np.lib.stride_tricks.sliding_window_view(s, size)[onsets, :]
+    # matrix = np.lib.stride_tricks.sliding_window_view(s, size)[onsets[mask], :]
+    return matrix, onsets, mask
