1ab919dca221f457656be21fa15415ff0a254ce6
sfcaracciolo <santiago.caracciolo@gmail.com>
2022-02-03 14:19:09 -0300

    fix sliding window

--- ecg_tools/utils.py
+++ ecg_tools/utils.py
@@ -1,9 +1,9 @@
 import numpy as np 
 
 def sliding_window_from_centers(s: np.ndarray, centers: np.ndarray, size: int) -> tuple:
-    onsets = centers - size // 2
+    matrix_rows = s.size - size + 1
+    onsets = np.mod(centers - size // 2, matrix_rows)
     mask = (onsets >= 0) & (onsets <= s.size - size)
-    np.logical_not(mask, out=mask)
+    filter = np.logical_not(mask)
     matrix = np.lib.stride_tricks.sliding_window_view(s, size)[onsets, :]
-    # matrix = np.lib.stride_tricks.sliding_window_view(s, size)[onsets[mask], :]
-    return matrix, onsets, mask
+    return matrix, onsets, filter
